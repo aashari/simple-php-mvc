364d44c30559cc9e42fa611d452b3441e95bf041
Andi Muqsith Ashari <mq.aashari@gmail.com>
2018-02-10 17:02:20 +0700

    Update routing logic

--- models/Model.php
+++ models/Model.php
@@ -2,54 +2,8 @@
 
 class Model {
 
-	protected $mongodb;
-	protected $mongodbConnection;
-	protected $collection;
-	
 	public function __construct(){
-		$config = config();
-		$this->mongodbConnection = new MongoDB\Client('mongodb://'.$config->database->host,[
-				'username' => $config->database->user,
-				'password' => $config->database->pass,
-				'database' => $config->database->name
-			]);
-		$this->mongodb = $this->mongodbConnection->{$config->database->name};
+		
 	}
 	
-	public function insert($data)
-	{
-		try{
-			return $this->mongodb->{$this->collection}->insertOne($data);
-		}catch(\Exception $e){
-			return null;
-		}
-	}
-	
-	public function update($condition, $data)
-	{
-		try{
-			return $this->mongodb->{$this->collection}->updateMany($condition,[
-				'$set' => $data
-			]);
-		}catch(\Exception $e){
-			return null;
-		}
-	}
-	
-	public function get($condition){
-		try{
-			return $this->mongodb->{$this->collection}->findOne($condition);
-		}catch(\Exception $e){
-			return null;
-		}
-	}
-	
-	public function getList($condition){
-		try{
-			return $this->mongodb->{$this->collection}->find($condition);
-		}catch(\Exception $e){
-			return null;
-		}
-	}
-    
 }
--- public/index.php
+++ public/index.php
@@ -2,49 +2,29 @@
 
 require_once("../vendor/autoload.php");
 
+$routes = [];
+
+require_once("../router.php");
+
 #get client access url
 $uri = @$_SERVER['REDIRECT_URL']?$_SERVER['REDIRECT_URL']:@$_SERVER['PATH_INFO'];
 #replace duplicate slash
 $uri = preg_replace('/(\/+)/','/',$uri);
 $uri = ltrim(rtrim($uri,'/'),'/');
-#explode url by slash
-$uri = explode('/', $uri);
 
 #getHTTPMethod 
 $requestMethod = @$_SERVER['REQUEST_METHOD'];
 $requestMethod = strtolower($requestMethod);
 if(empty($requestMethod)) $requestMethod = "get";
 
-#setting default controller and method
-$controller = "home";
-$method = "index";
-
-#uri to controller
-if(@$uri[0]) {
-    $controller = $uri[0];
-    unset($uri[0]);
-}
-
-#uri to method
-if(@$uri[1]) {
-    $method = $uri[1];
-    unset($uri[1]);
-}
-
-#get the reset of uri as parameters
-$parameters = array_values($uri);
+$currentRoute = $routes[$uri][$requestMethod];
+$currentRoute = explode('@',$currentRoute);
 
 #translate the controller
-$controller = str_replace('-', ' ', $controller);
-$controller = ucwords($controller);
-$controller = str_replace(' ','',$controller);
-$controller = $controller."Controller";
+$controller = $currentRoute[0];
 
 #translate the method
-$method = str_replace('-', ' ', $method);
-$method = ucwords($method);
-$method = str_replace(' ','',$method);
-$method = lcfirst($method);
+$method = $currentRoute[1];
 
 #require controller
 $path = "../controllers/".$controller.".php";
@@ -57,13 +37,7 @@ require_once("../models/Model.php");
 if(file_exists($path)){
     require_once($path);
     $controller = new $controller();
-    
-    if(method_exists($controller,$requestMethod.$method)){
-        echo $controller->{$requestMethod.$method}($parameters);    
-    }else{
-        echo $controller->$method($parameters);
-    }
-    
+    echo $controller->$method($parameters);
 }else{
     echo "Controller not found!";
     exit();
